6f05fc5f174e6812e7af258f1b96ed79f71a1851
Nithish Reddy <43609207+kittureddy05@users.noreply.github.com>
2024-08-10 09:47:21 +0000

    updated

--- dbtlearn/docs/compiled/dbtlearn/models/fct/fct_review.sql
+++ dbtlearn/docs/compiled/dbtlearn/models/fct/fct_review.sql
@@ -17,17 +17,7 @@ FROM
 ),src_reviews AS (
  SELECT * FROM __dbt__cte__src_reviews
 )
-SELECT  md5(cast(coalesce(cast(listing_id as 
-    varchar
-), '') || '-' || coalesce(cast(review_date as 
-    varchar
-), '') || '-' || coalesce(cast(reviewer_name as 
-    varchar
-), '') || '-' || coalesce(cast(review_text as 
-    varchar
-), '') as 
-    varchar
-)) as review_id,* FROM src_reviews
+SELECT  md5(cast(coalesce(cast(listing_id as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(review_date as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(reviewer_name as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(review_text as TEXT), '_dbt_utils_surrogate_key_null_') as TEXT)) as review_id,* FROM src_reviews
 WHERE review_text is not null
 
  AND review_date > (select max(review_date) from AIRBNB.DEV.fct_review)
